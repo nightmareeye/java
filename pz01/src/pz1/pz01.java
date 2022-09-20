7e5842af8bb4403b426674925de5c0ba54e0ec9a
foxmulder <foxmulder@gitwork.ru>
2022-09-20 12:12:50 +0300

    Done more tasks

--- pz01/src/pz1/pz01.java
+++ pz01/src/pz1/pz01.java
@@ -44,9 +44,51 @@ public class pz01 {
 		}
 		return mid;
 	}
+	
+	public static void table() {
+		int[][] a = new int[3][5];
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<5; j++) {
+				a[i][j]=(int)(10*Math.random()%(10));
+			}
+		}
+		
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<5; j++) {
+				System.out.print(a[i][j] + "  ");
+			}
+			System.out.print("\n");
+		}
+		
+	}
+	
+	public static void scalar() {
+		double[] a,b,c;
+		a=new double[]{10e20, 1223, 10e18, 10e15, 3, -10e12};
+		b=new double[]{10e20, 2, -10e22, 10e13, 2111, 10e16};
+		//c=new double[3];
+		// Индексная переменная:
+		int k;
+		// Переменная для записи скалярного произведения:
+		double s=0;
+		//System.out.print("Произведение векторов:\n[a.b]=<");
+		// Вычисление и вывод на экран результата:
+		for(k=0;k<6;k++){
+			s+=a[k]*b[k];
+			//c[k]=a[(k+1)%3]*b[(k+2)%3]-a[(k+2)%3]*b[(k+1)%3];
+			//System.out.print(c[k]+(k!=2?";":">\n"));
+		}
+			System.out.println("a.b="+s);
+	}
+	
 	public static void main(String[] args) {
 		pyram();
 		change();
-		System.out.print(mid());
+		System.out.println(mid());
+		table();
+		scalar();
+		System.out.print(10e10);
 	}
 }
