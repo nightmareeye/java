961ddc6f6c4f98d6c0cdc823d22eb5edaa3b9825
foxmulder <foxmulder@gitwork.ru>
2022-10-08 14:04:10 +0300

    Created dot class

--- pz02/src/pz02/dot.java
+++ pz02/src/pz02/dot.java
@@ -1,5 +1,23 @@
 package pz02;
 
-public class dot {
-
+public class Dot {
+	public int x, y;
+	
+	public Dot() {
+		x=0;
+		y=0;
+	}
+	public Dot(int xx, int yy) {
+		x=xx;
+		y=yy;
+	}
+	
+	public String toString() { 
+		return "dot with coords: ("+x+","+y+")";
+	}
+	
+	public boolean equals(Dot a, Dot b) {
+		if((a.x==b.x)&&(a.y==b.y)) return true;
+		else return false;
+	}
 }
