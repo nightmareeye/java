c0c660928d0caede34b5a7ca0e64e4e6e7a04310
foxmulder <foxmulder@gitwork.ru>
2022-10-20 10:05:38 +0300

    Halfway on pz02

--- pz02/src/pz02/Circle.java
+++ pz02/src/pz02/Circle.java
@@ -10,9 +10,23 @@ public class Circle {
 		point.y=0;
 		rad=0;
 	}
+	
 	public Circle(Dot center, int radius) {
 		point.x=center.x;
 		point.y=center.y;
 		rad = radius;
+	}
+	
+	public double square(){
+		return 3.14*3.14*rad;
+	}
+	
+	public String toString() {
+		 return "Circle with center ("+point.x+","+point.y+") with radius "+rad;
+	}
+	
+	public Boolean equals(Circle c1, Circle c2){
+		if ((c1.point.x==c2.point.x)&&(c1.point.y==c2.point.y)&&(c1.rad==c2.rad)) return true;
+		else return false;
 	}
 }
