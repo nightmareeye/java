70d002c674f010a4345e639b385d2fab964d22a6
foxmulder <foxmulder@gitwork.ru>
2022-11-03 10:22:19 +0300

    Created tests

---
+++ pz01/src/pz1/test.java
@@ -0,0 +1,14 @@
+package pz1;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class test extends TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
